a33db6566e40249a2bc9d3d633cfec26225f4c5a
PawelBorowiecki <pawelbor77@gmail.com>
2025-07-04 22:30:37 +0200

    Methods in UserServiceImpl implemented except register

--- src/main/java/com/pawel/musicshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/pawel/musicshop/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.pawel.musicshop.service.impl;
 
 import com.pawel.musicshop.dto.UserRequest;
+import com.pawel.musicshop.model.Role;
 import com.pawel.musicshop.model.User;
+import com.pawel.musicshop.repository.RoleRepository;
+import com.pawel.musicshop.repository.UserRepository;
 import com.pawel.musicshop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,38 +15,60 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     @Override
     public void register(UserRequest req) {
-
+        //TODO gdy bedzie security
     }
 
     @Override
     public Optional<User> findById(String id) {
-        return Optional.empty();
+        return userRepository.findById(id);
     }
 
     @Override
     public Optional<User> findByLogin(String login) {
-        return Optional.empty();
+        return userRepository.findByLogin(login);
     }
 
     @Override
     public void deleteById(String id) {
-
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            user.get().setActive(false);
+            userRepository.save(user.get());
+        }
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public boolean giveRole(String userId, String roleName) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role> role = roleRepository.findByName(roleName);
+        if(user.isPresent() && role.isPresent()){
+            if(user.get().isActive()){
+                user.get().getRoles().add(role.get());
+                userRepository.save(user.get());
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void divestRole(String userId, String roleName) {
-
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role> role = roleRepository.findByName(roleName);
+        if(user.isPresent() && role.isPresent()){
+            if(user.get().isActive()){
+                user.get().getRoles().remove(role.get());
+                userRepository.save(user.get());
+            }
+        }
     }
 }
