fc021909f51a5f706f4ab73bceb445b95d494c47
PawelBorowiecki <pawelbor77@gmail.com>
2025-07-04 21:49:16 +0200

    Finding user's cart added in CartRepository

--- src/main/java/com/pawel/musicshop/repository/CartRepository.java
+++ src/main/java/com/pawel/musicshop/repository/CartRepository.java
@@ -2,8 +2,13 @@ package com.pawel.musicshop.repository;
 
 import com.pawel.musicshop.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, String> {
+    @Query("SELECT c FROM Cart c WHERE c.user.id = ?1")
+    public Optional<Cart> findUserCart(String userId);
 }
