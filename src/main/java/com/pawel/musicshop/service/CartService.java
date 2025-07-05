b5c5c7d85fab1169b17810b3b218770f6b7560e0
PawelBorowiecki <pawelbor77@gmail.com>
2025-07-05 18:47:17 +0200

    Adding and deleting products from Cart added

--- src/main/java/com/pawel/musicshop/service/CartService.java
+++ src/main/java/com/pawel/musicshop/service/CartService.java
@@ -9,4 +9,6 @@ public interface CartService {
     List<Cart> findAll();
     Optional<Cart> findById(String id);
     Optional<Cart> findUserCart(String userId);
+    boolean addMusicCDToCart(String cdId, String userId);
+    boolean deleteMusicCDFromCart(String cdId, String userId);
 }
--- src/main/java/com/pawel/musicshop/service/impl/CartServiceImpl.java
+++ src/main/java/com/pawel/musicshop/service/impl/CartServiceImpl.java
@@ -1,7 +1,11 @@
 package com.pawel.musicshop.service.impl;
 
 import com.pawel.musicshop.model.Cart;
+import com.pawel.musicshop.model.MusicCD;
+import com.pawel.musicshop.model.User;
 import com.pawel.musicshop.repository.CartRepository;
+import com.pawel.musicshop.repository.MusicCDRepository;
+import com.pawel.musicshop.repository.UserRepository;
 import com.pawel.musicshop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
     private final CartRepository cartRepository;
+    private final MusicCDRepository musicCDRepository;
+    private final UserRepository userRepository;
     @Override
     public List<Cart> findAll() {
         return cartRepository.findAll();
@@ -26,5 +32,35 @@ public class CartServiceImpl implements CartService {
     @Override
     public Optional<Cart> findUserCart(String userId) {
         return cartRepository.findUserCart(userId);
+    }
+
+    @Override
+    public boolean addMusicCDToCart(String cdId, String userId) {
+        Optional<MusicCD> musicCD = musicCDRepository.findById(cdId);
+        Optional<Cart> cart = cartRepository.findUserCart(userId);
+        if(musicCD.isPresent() && cart.isPresent()){
+            if(musicCD.get().isInCart()){
+                return false;
+            }else{
+                cart.get().getProducts().add(musicCD.get());
+                musicCD.get().setInCart(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteMusicCDFromCart(String cdId, String userId) {
+        Optional<MusicCD> musicCD = musicCDRepository.findById(cdId);
+        Optional<Cart> cart = cartRepository.findUserCart(userId);
+        if(musicCD.isPresent() && cart.isPresent()){
+            if(musicCD.get().isInCart() && cart.get().getProducts().contains(musicCD.get())){
+                musicCD.get().setInCart(false);
+                cart.get().getProducts().remove(musicCD.get());
+                return true;
+            }
+        }
+        return false;
     }
 }
